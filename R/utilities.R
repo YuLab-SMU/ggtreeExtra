7a5a727478fcece618168a5506f0f00343fe9c9d
xiangpin <xshuangbin@163.com>
2020-05-29 20:55:34 +0800

    modified min value

--- R/utilities.R
+++ R/utilities.R
@@ -13,6 +13,9 @@ pos_dodge <- getFromNamespace("pos_dodge", "ggplot2")
 normxy <- function(refnum, targetnum, na.rm=TRUE, ratio=0.38){
     rmax <- max(refnum, na.rm=na.rm) * ratio
     rmin <- min(refnum, na.rm=na.rm)
+    if (rmin==0){
+        rmin <- rmax/10000000
+    }
     tmax <- max(targetnum, na.rm=na.rm)
     tmin <- min(targetnum, na.rm=na.rm)
     k <- (rmax - rmin)/(tmax - tmin)
