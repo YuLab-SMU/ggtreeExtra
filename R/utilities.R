bab443495e007abd13ec2e28af9be7665b2ef58c
xiangpin <xshuangbin@163.com>
2020-05-29 22:46:34 +0800

    modified the lower bound range of normalization

--- R/utilities.R
+++ R/utilities.R
@@ -12,10 +12,7 @@ pos_dodge <- getFromNamespace("pos_dodge", "ggplot2")
 
 normxy <- function(refnum, targetnum, na.rm=TRUE, ratio=0.38){
     rmax <- max(refnum, na.rm=na.rm) * ratio
-    rmin <- min(refnum, na.rm=na.rm)
-    if (rmin==0){
-        rmin <- rmax/10000000
-    }
+    rmin <- min(refnum[refnum!=0], na.rm=na.rm)
     tmax <- max(targetnum, na.rm=na.rm)
     tmin <- min(targetnum, na.rm=na.rm)
     k <- (rmax - rmin)/(tmax - tmin)
