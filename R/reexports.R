731e1b47927894eedbb29a3b760c6d215bca8892
xiangpin <xshuangbin@163.com>
2020-06-05 20:22:38 +0800

    reexport new_scale from ggnewscale

--- R/reexports.R
+++ R/reexports.R
@@ -5,3 +5,15 @@ magrittr::`%>%`
 ##' @importFrom ggplot2 ggplot_add
 ##' @export
 ggplot2::ggplot_add
+
+##' @importFrom ggnewscale new_scale
+ggnewscale::new_scale
+
+##' @importFrom ggnewscale new_scale_fill
+ggnewscale::new_scale_fill
+
+##' @importFrom ggnewscale new_scale_color
+ggnewscale::new_scale_color
+
+##' @importFrom ggnewscale new_scale_colour
+ggnewscale::new_scale_colour
