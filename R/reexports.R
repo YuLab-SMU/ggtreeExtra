23607f47855afaefb613cdc7f2f5009a4d104d16
xiangpin <xshuangbin@163.com>
2020-07-10 11:20:41 +0800

    new reexport

--- R/reexports.R
+++ R/reexports.R
@@ -1,23 +1,4 @@
-##' @importFrom magrittr %>%
-##' @export
-magrittr::`%>%` 
-
 ##' @importFrom ggplot2 ggplot_add
 ##' @export
 ggplot2::ggplot_add
 
-##' @importFrom ggnewscale new_scale
-##' @export
-ggnewscale::new_scale
-
-##' @importFrom ggnewscale new_scale_fill
-##' @export
-ggnewscale::new_scale_fill
-
-##' @importFrom ggnewscale new_scale_color
-##' @export
-ggnewscale::new_scale_color
-
-##' @importFrom ggnewscale new_scale_colour
-##' @export
-ggnewscale::new_scale_colour
