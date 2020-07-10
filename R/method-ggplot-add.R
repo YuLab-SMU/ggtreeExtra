653f1fc2b4cf9ea28ca1699e8cb433c423d9c781
xiangpin <xshuangbin@163.com>
2020-07-10 11:21:03 +0800

    add importFrom

--- R/method-ggplot-add.R
+++ R/method-ggplot-add.R
@@ -2,6 +2,7 @@
 ##' @importFrom utils modifyList
 ##' @importFrom ggplot2 aes aes_ aes_string geom_vline scale_color_manual
 ##' @importFrom rlang as_name
+##' @importFrom ggnewscale new_scale_color
 ##' @author Shuangbin Xu
 ##' @export
 ggplot_add.fruit_plot <- function(object, plot, object_name){
