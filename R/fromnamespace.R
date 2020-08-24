4f0ac69109c9ded52617f49136d86b3f0c1cc34a
xiangpin <xshuangbin@163.com>
2020-08-24 21:16:43 +0800

    the funtion of third packages which not be exported

---
+++ R/fromnamespace.R
@@ -0,0 +1,16 @@
+#' @importFrom utils getFromNamespace
+stack_var <- getFromNamespace("stack_var", "ggplot2")
+
+collide <- getFromNamespace("collide", "ggplot2")
+
+collide2 <- getFromNamespace("collide2", "ggplot2")
+
+pos_stack <- getFromNamespace("pos_stack", "ggplot2")
+
+pos_dodge <- getFromNamespace("pos_dodge", "ggplot2")
+
+"%||%" <- getFromNamespace("%||%", "ggplot2")
+
+find_x_overlaps <- getFromNamespace("find_x_overlaps", "ggplot2")
+
+pos_dodge2 <- getFromNamespace("pos_dodge2", "ggplot2")
