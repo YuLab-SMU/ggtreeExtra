24e9555ea87b0757a32958783ab445f60267de75
xiangpin <xshuangbin@163.com>
2021-03-24 16:33:22 +0800

    update help information of rd

--- R/geom_fruit.R
+++ R/geom_fruit.R
@@ -1,38 +1,29 @@
-##' plot tree associated data in another method.
+##' @title plot tree with associated data in another method.
 ##'
+##' @description 
+##' 'geom_fruit()' can automatically re-arrange the input 'data' according to the tree structure.
+##' It can present the associated data on the external panels of the tree using the 'geom' function defined 
+##' in 'ggplot2' or other ggplot2-based packages with aesthetic 'mapping' and other parameters, 
+##' and it will align the external layers in the outer ring of circular layout tree or with rectangular 
+##' layout tree side by side. Note: the tree should be created by 'ggtree'.
 ##'
-##' 'geom_fruit()' automatically re-arranges the input 'data' according to the tree structure,
-##' visualizes the 'data' on specific 'panel' using the 'geom' function with aesthetic 'mapping' 
-##' and other parameters, and align the graph with the tree 'p' side by side. 
-##' The default position parameters is 'auto'. If you want to set manually. You can use 
-##' 'position_stackx()' or 'position_dodgex()' for 'geom_bar', 'position_identityx()' for 
-##' 'geom_tile', 'geom_point', 'geom_star', 'geom_symbol' or other layers using 'identity' 
-##' position in 'ggplot2', and 'position_dodgex()' or 'position_dodgex2()' for 'geom_boxplot'
-##' 'geom_violin' or other layers using 'dodge' position in 'ggplot2'. The axis line and text 
-##' can be added using 'axis.params=list(axis="x",...)', and the grid line also can be added
-##' using 'grid.params=list(...)'.
-##'
-##' @title geom_fruit
 ##' @rdname geom_fruit
 ##' @param p tree view
 ##' @param mapping aes mapping for 'geom' 
 ##' @param data data to plot by 'geom', the column contained tree tip labels 
 ##' should be as y in mapping.
 ##' @param geom geom function to plot the data.
-##' @param offset numeric, distance between panels, the ratio of distance 
-##' to tree, default is 0.03.
-##' @param pwidth numeric, the width of new geom layer, this is the ratio of the 
-##' new geom to tree, default is 0.2.
+##' @param offset numeric, distance between external layers or between tree and external layers,
+##' default is 0.03, meaning the 0.03 times of x range of tree (0.03 * xrange of tree).
+##' @param pwidth numeric, the width of external geometric layer, default is 0.2, 
+##' meaning the 0.2 times of x range of tree (0.2 * xrange of tree).
 ##' @param position Position adjustment, either as a string, or the result of a
-##' call to a position adjustment function, default is 'auto'.
+##' call to a position adjustment function, default is 'auto', see details in the following.
 ##' @param grid.params list, the parameters to control the attributes of grid lines, 
-##' default is NULL.
-##' @param axis.params list, the parameters to control the attributes of pseudo axis.
-##' @param ... additional parameters for 'geom'
-##' 
-##' grid.params control the attributes of grid line, it can be referred to the
-##' following parameters:
-##'     \itemize{
+##' default is NULL, see the grid.params in the following.
+##'
+##' grid.params control the attributes of grid line of external layer, it can be referred to the
+##' following parameters:                                                                                                                                                                                          ##'     \itemize{
 ##'         \item \code{vline} logical, whether add the vertical line, default is FALSE.
 ##'         \item \code{color} color of line, default is grey.
 ##'         \item \code{size} the width of line, default is 0.2.
@@ -40,13 +31,14 @@
 ##'         \item \code{lineend} Line end style (round, butt, square), default is "butt".
 ##'         \item \code{linejoin} Line end style (round, butt, square), default is "round".
 ##'         \item \code{linetype} Type of line, default is 1.
-##'     } 
+##'     }
+##' @param axis.params list, the parameters to control the attributes of pseudo axis,
+##' see the axis.params in the following.
 ##'
-##' axis.params control the attributes of pseudo axis, it can be referred to the
-##' following parameters:
+##' axis.params control the attributes of axis, it can be referred to the following parameters:
 ##'     \itemize{
-##'         \item \code{axis} character, add the pseudo axis, "none" don't display axis (default), 
-##'         "x" display the x axis, "y" display the (y) axis of tree tip, "xy" display the two axis.
+##'         \item \code{axis} character, add the axis, if it is set to "none", meaning don't display axis (default),
+##'         "x" display the x axis, "y" display the y axis, "xy" display the two axis.
 ##'         \item \code{text} vector, the text of axis x, default is NULL, it is only valid when
 ##'         the text of axis is single and x is discrete.
 ##'         \item \code{vjust} numeric, A numeric specifying vertical justification, default is 0.5.
@@ -60,14 +52,51 @@
 ##'          it is relative to height of tree.
 ##'         \item \code{title.angle} numeric, the angle of title text, default is 0.
 ##'         \item \code{title.color} character, the color of title text, default is "black".
-##'         \item \code{nbreak} numeric, meaning the number of axis to break, 
+##'         \item \code{nbreak} numeric, meaning the number of axis to break,
 ##'          integer giving the _desired_ number of intervals. Non-integer values are rounded down.
 ##'          It is only valid when x is continuous, default is 4.
 ##'         \item \code{line.size} numeric, the size of axis line, default is 0.2.
-##'         \item \code{line.color} character, the color of axis color, default is "grey".
-##'         \item \code{line.alpha} numeric, the colour transparency of line, default is 1.
+##'         \item \code{line.color} character, the color of axis line color, default is "grey".
+##'         \item \code{line.alpha} numeric, the colour transparency of axis line, default is 1.
 ##'     }
+##' @param ... additional parameters for 'geom'
+##' 
+##' @details
 ##'
+##' The 'data' parameter is data.frame or tibble type, it is the same with the data for corresponding
+##' geometric layers, but it must contain one column of taxa labels of tree, it will be mapped to 'y' axis
+##' in 'mapping'.
+##'
+##' The 'mapping' parameter is setting of aesthetic mappings created by 'aes()' or 'aes_()' of 'ggplot2',
+##' the y should be assigned to the variable names of column of taxa labels in data.frame of 'data'.
+##'
+##' The 'geom' parameter is the geometric function defined in 'ggplot2' or other 'ggplot2-extension',
+##' e.g.
+##'  \tabular{lll}{
+##'    ggplot2 \tab geom_bar,geom_col,geom_boxplot,geom_violin,geom_tile \tab circular, rectangular \cr
+##'    ggmsa   \tab geom_msa \tab rectangular \cr
+##'    ggstar  \tab geom_star \tab circular, rectangular \cr
+##'    ggimage \tab geom_image,geom_phylopic \tab circular, rectangular \cr
+##'    ggpmisc \tab geom_plot,geom_table \tab circular, rectangular \cr
+##'    ggridges \tab geom_density_ridges \tab circular, rectangular \cr
+##'    ggtext  \tab geom_richtext \tab circular, rectangular \cr
+##'    ...   
+##' }
+##' if the 'geom' is 'geom_bar', 'geom_col', 'geom_boxplot', 'geom_violin', the 'orientation' should be
+##' specified to 'y'.
+##'
+##' The default 'position' parameter is 'auto', it will guess and determine (hopefully) a suitable
+##' position for the specified geometric layer. That means using 'position_stackx()' for geom_bar(),
+##' 'position_dodgex()' for 'geom_violin()' and 'geom_boxplot()', and 'position_identityx()' for
+##' others (e.g. geom_point() and geom_tile() etc.). A geometric layer that has a position parameter
+##' should be compatible with geom_fruit(), as it allows using position functions defined in the
+##' ggtreeExtra package to adjust output layer position.
+##'
+##' and the grid line also can be added using 'grid.params=list(...)'.
+##'
+##' The axis line and text can be added using 'axis.params=list(axis="x",...)'.
+##'
+##' The 'p' parameter only work when you use `fruit_plot()`, which is alias of `geom_fruit()`.
 ##' @return ggplot object
 ##' @export
 ##' @author Shuangbin Xu and Guangchuang Yu
