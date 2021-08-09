fa31c18694d8ee7200b6f11d21fb82e1f3246110
xiangpin <xshuangbin@163.com>
2021-08-09 17:04:37 +0800

    fix compute_aes

--- R/utilities-ggplot-add.R
+++ R/utilities-ggplot-add.R
@@ -6,6 +6,10 @@ check_subset_aes <- function(object){
     return(object)
 }
 
+defaults <- function (x, y){
+    c(x, y[setdiff(names(y), names(x))])
+}
+
 #' @importFrom rlang quo_text
 compute_aes <- function(object, plot){
     if("x" %in% names(object$mapping) && !quo_text(object$mapping$x) %in% colnames(object$data)){
@@ -13,6 +17,15 @@ compute_aes <- function(object, plot){
         reqaes <- extract_requierd_aes(object)
         mappingx <- mappingx[names(mappingx) %in% reqaes]
         geomobj <- do.call(object$geom, list(mappingx))
+        if (utils::packageVersion("ggplot2") > '3.3.3'){
+            # the 3.3.4 > version of ggplot2 introduces computed_mapping argument. 
+            if (isTRUE(object$inherit.aes)) {
+                geomobj$computed_mapping <- defaults(geomobj$mapping, plot$mapping)
+                class(geomobj$computed_mapping) <- "uneval"
+            } else {
+                geomobj$computed_mapping <- geomobj$mapping
+            }
+        }
         data_after_cal <- suppressWarnings(geomobj$compute_aesthetics(data=object$data, plot=plot))
         object$data[["x"]] <- data_after_cal[["x"]]
         object$mapping <- modifyList(object$mapping, aes_string(x="x"))
