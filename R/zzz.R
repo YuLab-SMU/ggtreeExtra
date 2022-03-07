3de0e5834efede3e410787a20d9687be19682def
xiangpin <xshuangbin@163.com>
2022-03-07 11:26:47 +0800

    update print of refs

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,8 @@
 #' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
     pkgVersion <- packageDescription(pkgname, fields="Version")
-    msg <- paste0(pkgname, " v", pkgVersion, "  ",
-                  "For help: https://yulab-smu.top/treedata-book/", "\n\n")
-
-    citation <- paste0("If you use ", pkgname,
-                       " in published research, please cite the paper:\n\n",
-                       ggtreeExtra_citations())
-
-    packageStartupMessage(paste0(strwrap(pillar::style_subtle(paste0(msg, citation))), collapse="\n"))
+    ref <- random_ref(pkgname = pkgname, pkgVersion = pkgVersion, random_n = 0)
+    if (!is.null(ref)) packageStartupMessage(ref)    
 }
+
+random_ref <- getFromNamespace("random_ref", 'tidytree')
