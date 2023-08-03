221b2853e9e414665e0d9c754f5bac62165da9ab
xiangpin <xshuangbin@163.com>
2023-08-03 15:26:18 +0800

    update citation format

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 #' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
     pkgVersion <- packageDescription(pkgname, fields="Version")
-    ref <- random_ref(pkgname = pkgname, pkgVersion = pkgVersion, random_n = 0)
+    ref <- random_ref(pkgname = pkgname, pkgVersion = pkgVersion, random_n = 2)
     if (!is.null(ref)) packageStartupMessage(ref)    
 }
 
